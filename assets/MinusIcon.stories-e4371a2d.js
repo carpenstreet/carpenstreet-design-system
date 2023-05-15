57df83782b76a34fd284be0f26778e78dac1624e
obzva <dngyng1000@gmail.com>
2023-05-15 14:37:00 +0900

    Updates

--- assets/MinusIcon.stories-e4371a2d.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./jsx-runtime-91a467a5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const s=({size:r=24})=>t("svg",{width:r,height:r,viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:t("path",{d:"M19 13H5C4.44772 13 4 12.5523 4 12C4 11.4477 4.44772 11 5 11H19C19.5523 11 20 11.4477 20 12C20 12.5523 19.5523 13 19 13Z",fill:"#313135"})});try{s.displayName="MinusIcon",s.__docgenInfo={description:"각종 UI 요소에 사용되는 '마이너스' 아이콘입니다.",displayName:"MinusIcon",props:{size:{defaultValue:{value:"24"},description:"",name:"size",required:!1,type:{name:"number"}}}}}catch{}const p={title:"Element/Icon/Sign/MinusIcon",component:s,tags:["autodocs"]},e={args:{size:24}};var o,n,u;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    size: 24
-  }
-}`,...(u=(n=e.parameters)==null?void 0:n.docs)==null?void 0:u.source}}};const C=["Default"];export{e as Default,C as __namedExportsOrder,p as default};
-//# sourceMappingURL=MinusIcon.stories-e4371a2d.js.map
--- assets/PersonalIcon.stories-ae82be7f.js
+++
@@ -1,6 +0,0 @@
-import{a as i,j as t}from"./jsx-runtime-91a467a5.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";const o=({size:r=24})=>i("svg",{width:r,height:r,viewBox:"0 0 24 24",fill:"none",xmlns:"http://www.w3.org/2000/svg",children:[t("rect",{width:"24",height:"24",rx:"4",fill:"#FFD1F6"}),t("rect",{x:"8.5",y:"4.61841",width:"7",height:"7",rx:"3.5",fill:"white",stroke:"#313135",strokeLinejoin:"round"}),t("path",{d:"M4.5164 18.6184C4.7737 14.7094 8.02588 11.6184 12 11.6184C15.9741 11.6184 19.2263 14.7094 19.4836 18.6184H4.5164Z",fill:"white",stroke:"#313135",strokeLinejoin:"round"})]});try{o.displayName="PersonalIcon",o.__docgenInfo={description:"'개인 사용권' 아이콘입니다.",displayName:"PersonalIcon",props:{size:{defaultValue:{value:"24"},description:"",name:"size",required:!1,type:{name:"number"}}}}}catch{}const d={title:"Element/Icon/Product/PersonalIcon",component:o,tags:["autodocs"]},e={args:{size:24}};var s,n,a;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {
-    size: 24
-  }
-}`,...(a=(n=e.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,d as default};
-//# sourceMappingURL=PersonalIcon.stories-ae82be7f.js.map
