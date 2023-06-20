ecd5124d4409430404eca95e3611c286f925d26f
sanguneo <sanguneo@acon3d.com>
2023-06-20 11:51:33 +0900

    Updates

---
+++ assets/Letter.stories-ddb4c6b8.js
@@ -0,0 +1,8 @@
+import{j as s}from"./jsx-runtime-91a467a5.js";import{L as m}from"./Letter-6a4dedd8.js";import{T as n}from"./ThemeMui-ffd9c7ee.js";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./Color-e0bb3700.js";import"./extends-98964cd2.js";const g={title:"Foundation/Letter",component:m,tags:["autodocs"]},r={args:{variant:"letter/en, ko/medium",color:"color/red/500",children:"테스트 텍스트"}};r.decorators=[a=>s(n,{children:a()})];var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    variant: 'letter/en, ko/medium',
+    color: 'color/red/500',
+    children: '테스트 텍스트'
+  }
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const h=["Default"];export{r as Default,h as __namedExportsOrder,g as default};
+//# sourceMappingURL=Letter.stories-ddb4c6b8.js.map
