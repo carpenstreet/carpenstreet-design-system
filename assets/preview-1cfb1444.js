bf5da0917b62c4ba421f75042142745991dc975e
sanguneo <sanguneo@acon3d.com>
2023-06-22 16:00:25 +0900

    Updates

---
+++ assets/preview-1cfb1444.js
@@ -0,0 +1,2 @@
+import{_ as t}from"./iframe-2b6a423e.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-EYKKDMVH-9afb557b.js").then(e=>e.D),["./DocsRenderer-EYKKDMVH-9afb557b.js","./iframe-2b6a423e.js","./index-8db94870.js","./_commonjsHelpers-042e6b4d.js","./_getPrototype-c39e4407.js","./index-d475d2ea.js","./index-d37d4223.js","./extends-98964cd2.js","./index-356e4a49.js"],import.meta.url);return new r}}};export{_ as parameters};
+//# sourceMappingURL=preview-1cfb1444.js.map
