754d988a5a5a598b7d63cff17c8fbc745d204aee
flynn0909 <flynn@acon3d.com>
2024-03-08 16:50:23 +0900

    color 객체를 index를 통해 export 하지 않음
    - 나중에 필요해지면 하는 걸로

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,9 +3,8 @@ import Letter from '@components/Letter/Letter.tsx';
 import Shadow from '@components/Shadow/Shadow.tsx';
 import Icon from '@components/Icon/index.ts';
 import Typography from '@components/Typography/Typography.tsx';
-import { color } from '@shared/settings/Color.ts';
 
-export { Button, Letter, Shadow, Typography, Icon, color };
+export { Button, Letter, Shadow, Typography, Icon };
 export * from '@components/Icon/index.ts';
 
 export default {
@@ -15,5 +14,4 @@ export default {
   Typography,
   Icon,
   ...Icon,
-  color,
 };
