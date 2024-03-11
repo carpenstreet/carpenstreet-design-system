9c313025dc7b699aaa6b43550b11520f770ffbb7
flynn0909 <flynn@acon3d.com>
2024-03-11 09:35:27 +0900

    Typography constants에 기본적으로 설정된 color: '#313135' 삭제하고 Typography.tsx의 props에서 기본값으로 color/gray/800 설정

--- src/components/Typography/Typography.constants.ts
+++ src/components/Typography/Typography.constants.ts
@@ -1,28 +1,24 @@
 export const CustomTypographyVariants = {
   // h1
   'Typography/Headline/H1/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '40px',
     fontWeight: 800,
     lineHeight: '56px',
   },
   'Typography/Headline/H1/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '40px',
     fontWeight: 700,
     lineHeight: '56px',
   },
   'Typography/Headline/H1/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '40px',
     fontWeight: 600,
     lineHeight: '56px',
   },
   'Typography/Headline/H1/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '40px',
     fontWeight: 400,
@@ -31,28 +27,24 @@ export const CustomTypographyVariants = {
 
   // h2
   'Typography/Headline/H2/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '36px',
     fontWeight: 800,
     lineHeight: '48px',
   },
   'Typography/Headline/H2/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '36px',
     fontWeight: 700,
     lineHeight: '48px',
   },
   'Typography/Headline/H2/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '36px',
     fontWeight: 600,
     lineHeight: '48px',
   },
   'Typography/Headline/H2/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '36px',
     fontWeight: 400,
@@ -61,28 +53,24 @@ export const CustomTypographyVariants = {
 
   // h3
   'Typography/Headline/H3/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '32px',
     fontWeight: 800,
     lineHeight: '42px',
   },
   'Typography/Headline/H3/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '32px',
     fontWeight: 700,
     lineHeight: '42px',
   },
   'Typography/Headline/H3/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '32px',
     fontWeight: 600,
     lineHeight: '42px',
   },
   'Typography/Headline/H3/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '32px',
     fontWeight: 400,
@@ -91,28 +79,24 @@ export const CustomTypographyVariants = {
 
   // h4
   'Typography/Headline/H4/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '30px',
     fontWeight: 800,
     lineHeight: '40px',
   },
   'Typography/Headline/H4/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '30px',
     fontWeight: 700,
     lineHeight: '40px',
   },
   'Typography/Headline/H4/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '30px',
     fontWeight: 600,
     lineHeight: '40px',
   },
   'Typography/Headline/H4/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '30px',
     fontWeight: 400,
@@ -121,28 +105,24 @@ export const CustomTypographyVariants = {
 
   // title large
   'Typography/Title/Large/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '28px',
     fontWeight: 900,
     lineHeight: '36px',
   },
   'Typography/Title/Large/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '28px',
     fontWeight: 700,
     lineHeight: '36px',
   },
   'Typography/Title/Large/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '28px',
     fontWeight: 400,
     lineHeight: '36px',
   },
   'Typography/Title/Large/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '28px',
     fontWeight: 400,
@@ -151,28 +131,24 @@ export const CustomTypographyVariants = {
 
   // title medium
   'Typography/Title/Medium/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '24px',
     fontWeight: 800,
     lineHeight: '32px',
   },
   'Typography/Title/Medium/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '24px',
     fontWeight: 700,
     lineHeight: '32px',
   },
   'Typography/Title/Medium/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '24px',
     fontWeight: 600,
     lineHeight: '32px',
   },
   'Typography/Title/Medium/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '24px',
     fontWeight: 400,
@@ -181,28 +157,24 @@ export const CustomTypographyVariants = {
 
   // title small
   'Typography/Title/Small/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '20px',
     fontWeight: 900,
     lineHeight: '28px',
   },
   'Typography/Title/Small/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '20px',
     fontWeight: 700,
     lineHeight: '28px',
   },
   'Typography/Title/Small/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '20px',
     fontWeight: 600,
     lineHeight: '28px',
   },
   'Typography/Title/Small/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '20px',
     fontWeight: 400,
@@ -211,28 +183,24 @@ export const CustomTypographyVariants = {
 
   // body medium
   'Typography/Body/Medium/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '16px',
     fontWeight: 800,
     lineHeight: '24px',
   },
   'Typography/Body/Medium/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '16px',
     fontWeight: 700,
     lineHeight: '24px',
   },
   'Typography/Body/Medium/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '16px',
     fontWeight: 600,
     lineHeight: '24px',
   },
   'Typography/Body/Medium/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '16px',
     fontWeight: 400,
@@ -241,28 +209,24 @@ export const CustomTypographyVariants = {
 
   // body small
   'Typography/Body/Small/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '14px',
     fontWeight: 800,
     lineHeight: '20px',
   },
   'Typography/Body/Small/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '14px',
     fontWeight: 700,
     lineHeight: '20px',
   },
   'Typography/Body/Small/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '14px',
     fontWeight: 600,
     lineHeight: '20px',
   },
   'Typography/Body/Small/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '14px',
     fontWeight: 400,
@@ -271,28 +235,24 @@ export const CustomTypographyVariants = {
 
   // label Large
   'Typography/Label/Large/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '12px',
     fontWeight: 800,
     lineHeight: '16px',
   },
   'Typography/Label/Large/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '12px',
     fontWeight: 700,
     lineHeight: '16px',
   },
   'Typography/Label/Large/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '12px',
     fontWeight: 600,
     lineHeight: '16px',
   },
   'Typography/Label/Large/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '12px',
     fontWeight: 400,
@@ -301,28 +261,24 @@ export const CustomTypographyVariants = {
 
   // label Medium
   'Typography/Label/Medium/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '11px',
     fontWeight: 800,
     lineHeight: '14px',
   },
   'Typography/Label/Medium/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '11px',
     fontWeight: 700,
     lineHeight: '14px',
   },
   'Typography/Label/Medium/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '11px',
     fontWeight: 600,
     lineHeight: '14px',
   },
   'Typography/Label/Medium/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '11px',
     fontWeight: 400,
@@ -331,28 +287,24 @@ export const CustomTypographyVariants = {
 
   // label Small
   'Typography/Label/Small/black': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '10px',
     fontWeight: 800,
     lineHeight: '14px',
   },
   'Typography/Label/Small/bold': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '10px',
     fontWeight: 700,
     lineHeight: '14px',
   },
   'Typography/Label/Small/medium': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '10px',
     fontWeight: 600,
     lineHeight: '14px',
   },
   'Typography/Label/Small/regular': {
-    color: '#313135',
     fontFamily: 'Pretendard',
     fontSize: '10px',
     fontWeight: 400,
