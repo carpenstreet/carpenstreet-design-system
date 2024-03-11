ad02b20db69e1d70a17f43caede6788e09fa7149
flynn0909 <flynn@acon3d.com>
2024-03-11 15:51:11 +0900

    CustomColorType -> CustomColorPaletteType

--- src/components/Typography/Typography.types.ts
+++ src/components/Typography/Typography.types.ts
@@ -1,10 +1,10 @@
 import { TypographyTypeMap } from '@mui/material/Typography/Typography';
 import { CustomTypographyVariants } from '@components/Typography/Typography.constants';
-import { CustomColorTypes } from '@shared/settings/color.type';
+import { CustomColorPaletteTypes } from '@shared/settings/color.type';
 
 export type TypographyProps = Pick<TypographyTypeMap['props'], 'children'> & {
   variant: CustomTypographyVariantsTypes;
-  color?: CustomColorTypes;
+  color?: CustomColorPaletteTypes;
 };
 
 export type CustomTypographyVariantsTypes = keyof typeof CustomTypographyVariants;
