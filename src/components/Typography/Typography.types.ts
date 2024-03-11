69d8382a7e4612f4b05b91b1975981783cb6d4de
flynn0909 <flynn@acon3d.com>
2024-03-11 14:20:21 +0900

    Typography types 수정

--- src/components/Typography/Typography.types.ts
+++ src/components/Typography/Typography.types.ts
@@ -1,13 +1,9 @@
-import React from 'react';
 import { TypographyTypeMap } from '@mui/material/Typography/Typography';
 import { CustomTypographyVariants } from '@components/Typography/Typography.constants.ts';
+import { PaletteOptions } from '@mui/material';
 
-export type TypographyProps =
-  Pick<TypographyTypeMap['props'],
-    'children'
-  > & {
-  variant: CustomTypographyVariantsTypes;
-  color: string;
-}
+export type TypographyProps = Pick<TypographyTypeMap['props'], 'children' | 'variant'> & {
+  color?: keyof PaletteOptions;
+};
 
 export type CustomTypographyVariantsTypes = keyof typeof CustomTypographyVariants;
