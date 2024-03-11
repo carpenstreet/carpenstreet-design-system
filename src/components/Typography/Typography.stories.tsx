b9de4c24e9fcf5f595a72777a9ebd9403500a147
add0215 <mingeon@acon3d.com>
2024-03-11 14:15:17 +0900

    typography variant를 lowercase로 변경

--- src/components/Typography/Typography.stories.tsx
+++ src/components/Typography/Typography.stories.tsx
@@ -19,353 +19,353 @@ type Story = StoryObj<typeof Typography>;
 
 export const Default: Story = {
   args: {
-    variant: 'Typography/Headline/H1/regular',
+    variant: 'typography/headline/h1/regular',
     children: '내 인생의 전환점은 타이포그래피 수업이었다.',
   },
 };
 
 export const HeadlineH1Black: Story = {
   args: {
-    variant: 'Typography/Headline/H1/black',
-    children: 'Typography/Headline/H1/black',
+    variant: 'typography/headline/h1/black',
+    children: 'typography/headline/h1/black',
   },
 };
 
 export const HeadlineH1Bold: Story = {
   args: {
-    variant: 'Typography/Headline/H1/bold',
-    children: 'Typography/Headline/H1/bold',
+    variant: 'typography/headline/h1/bold',
+    children: 'typography/headline/h1/bold',
   },
 };
 
 export const HeadlineH1Medium: Story = {
   args: {
-    variant: 'Typography/Headline/H1/medium',
-    children: 'Typography/Headline/H1/medium',
+    variant: 'typography/headline/h1/medium',
+    children: 'typography/headline/h1/medium',
   },
 };
 
 export const HeadlineH1Regular: Story = {
   args: {
-    variant: 'Typography/Headline/H1/regular',
-    children: 'Typography/Headline/H1/regular',
+    variant: 'typography/headline/h1/regular',
+    children: 'typography/headline/h1/regular',
   },
 };
 // H2
 export const HeadlineH2Black: Story = {
   args: {
-    variant: 'Typography/Headline/H2/black',
-    children: 'Typography/Headline/H2/black',
+    variant: 'typography/headline/h2/black',
+    children: 'typography/headline/h2/black',
   },
 };
 
 export const HeadlineH2Bold: Story = {
   args: {
-    variant: 'Typography/Headline/H2/bold',
-    children: 'Typography/Headline/H2/bold',
+    variant: 'typography/headline/h2/bold',
+    children: 'typography/headline/h2/bold',
   },
 };
 
 export const HeadlineH2Medium: Story = {
   args: {
-    variant: 'Typography/Headline/H2/medium',
-    children: 'Typography/Headline/H2/medium',
+    variant: 'typography/headline/h2/medium',
+    children: 'typography/headline/h2/medium',
   },
 };
 
 export const HeadlineH2Regular: Story = {
   args: {
-    variant: 'Typography/Headline/H2/regular',
-    children: 'Typography/Headline/H2/regular',
+    variant: 'typography/headline/h2/regular',
+    children: 'typography/headline/h2/regular',
   },
 };
 
 // H3
 export const HeadlineH3Black: Story = {
   args: {
-    variant: 'Typography/Headline/H3/black',
-    children: 'Typography/Headline/H3/black',
+    variant: 'typography/headline/h3/black',
+    children: 'typography/headline/h3/black',
   },
 };
 
 export const HeadlineH3Bold: Story = {
   args: {
-    variant: 'Typography/Headline/H3/bold',
-    children: 'Typography/Headline/H3/bold',
+    variant: 'typography/headline/h3/bold',
+    children: 'typography/headline/h3/bold',
   },
 };
 
 export const HeadlineH3Medium: Story = {
   args: {
-    variant: 'Typography/Headline/H3/medium',
-    children: 'Typography/Headline/H3/medium',
+    variant: 'typography/headline/h3/medium',
+    children: 'typography/headline/h3/medium',
   },
 };
 
 export const HeadlineH3Regular: Story = {
   args: {
-    variant: 'Typography/Headline/H3/regular',
-    children: 'Typography/Headline/H3/regular',
+    variant: 'typography/headline/h3/regular',
+    children: 'typography/headline/h3/regular',
   },
 };
 
 // H4
 export const HeadlineH4Black: Story = {
   args: {
-    variant: 'Typography/Headline/H4/black',
-    children: 'Typography/Headline/H4/black',
+    variant: 'typography/headline/h4/black',
+    children: 'typography/headline/h4/black',
   },
 };
 
 export const HeadlineH4Bold: Story = {
   args: {
-    variant: 'Typography/Headline/H4/bold',
-    children: 'Typography/Headline/H4/bold',
+    variant: 'typography/headline/h4/bold',
+    children: 'typography/headline/h4/bold',
   },
 };
 
 export const HeadlineH4Medium: Story = {
   args: {
-    variant: 'Typography/Headline/H4/medium',
-    children: 'Typography/Headline/H4/medium',
+    variant: 'typography/headline/h4/medium',
+    children: 'typography/headline/h4/medium',
   },
 };
 
 export const HeadlineH4Regular: Story = {
   args: {
-    variant: 'Typography/Headline/H4/regular',
-    children: 'Typography/Headline/H4/regular',
+    variant: 'typography/headline/h4/regular',
+    children: 'typography/headline/h4/regular',
   },
 };
 
 // Title Large
 export const TitleLargeBlack: Story = {
   args: {
-    variant: 'Typography/Title/Large/black',
-    children: 'Typography/Title/Large/black',
+    variant: 'typography/title/large/black',
+    children: 'typography/title/large/black',
   },
 };
 
 export const TitleLargeBold: Story = {
   args: {
-    variant: 'Typography/Title/Large/bold',
-    children: 'Typography/Title/Large/bold',
+    variant: 'typography/title/large/bold',
+    children: 'typography/title/large/bold',
   },
 };
 
 export const TitleLargeMedium: Story = {
   args: {
-    variant: 'Typography/Title/Large/medium',
-    children: 'Typography/Title/Large/medium',
+    variant: 'typography/title/large/medium',
+    children: 'typography/title/large/medium',
   },
 };
 
 export const TitleLargeRegular: Story = {
   args: {
-    variant: 'Typography/Title/Large/regular',
-    children: 'Typography/Title/Large/regular',
+    variant: 'typography/title/large/regular',
+    children: 'typography/title/large/regular',
   },
 };
 
 // Title Medium
 export const TitleMediumBlack: Story = {
   args: {
-    variant: 'Typography/Title/Medium/black',
-    children: 'Typography/Title/Medium/black',
+    variant: 'typography/title/medium/black',
+    children: 'typography/title/medium/black',
   },
 };
 
 export const TitleMediumBold: Story = {
   args: {
-    variant: 'Typography/Title/Medium/bold',
-    children: 'Typography/Title/Medium/bold',
+    variant: 'typography/title/medium/bold',
+    children: 'typography/title/medium/bold',
   },
 };
 
 export const TitleMediumMedium: Story = {
   args: {
-    variant: 'Typography/Title/Medium/medium',
-    children: 'Typography/Title/Medium/medium',
+    variant: 'typography/title/medium/medium',
+    children: 'typography/title/medium/medium',
   },
 };
 
 export const TitleMediumRegular: Story = {
   args: {
-    variant: 'Typography/Title/Medium/regular',
-    children: 'Typography/Title/Medium/regular',
+    variant: 'typography/title/medium/regular',
+    children: 'typography/title/medium/regular',
   },
 };
 
 // Title Small
 export const TitleSmallBlack: Story = {
   args: {
-    variant: 'Typography/Title/Small/black',
-    children: 'Typography/Title/Small/black',
+    variant: 'typography/title/small/black',
+    children: 'typography/title/small/black',
   },
 };
 
 export const TitleSmallBold: Story = {
   args: {
-    variant: 'Typography/Title/Small/bold',
-    children: 'Typography/Title/Small/bold',
+    variant: 'typography/title/small/bold',
+    children: 'typography/title/small/bold',
   },
 };
 
 export const TitleSmallMedium: Story = {
   args: {
-    variant: 'Typography/Title/Small/medium',
-    children: 'Typography/Title/Small/medium',
+    variant: 'typography/title/small/medium',
+    children: 'typography/title/small/medium',
   },
 };
 
 export const TitleSmallRegular: Story = {
   args: {
-    variant: 'Typography/Title/Small/regular',
-    children: 'Typography/Title/Small/regular',
+    variant: 'typography/title/small/regular',
+    children: 'typography/title/small/regular',
   },
 };
 
 // Body Medium
 export const BodyMediumBlack: Story = {
   args: {
-    variant: 'Typography/Body/Medium/black',
-    children: 'Typography/Body/Medium/black',
+    variant: 'typography/body/medium/black',
+    children: 'typography/body/medium/black',
   },
 };
 
 export const BodyMediumBold: Story = {
   args: {
-    variant: 'Typography/Body/Medium/bold',
-    children: 'Typography/Body/Medium/bold',
+    variant: 'typography/body/medium/bold',
+    children: 'typography/body/medium/bold',
   },
 };
 
 export const BodyMediumMedium: Story = {
   args: {
-    variant: 'Typography/Body/Medium/medium',
-    children: 'Typography/Body/Medium/medium',
+    variant: 'typography/body/medium/medium',
+    children: 'typography/body/medium/medium',
   },
 };
 
 export const BodyMediumRegular: Story = {
   args: {
-    variant: 'Typography/Body/Medium/regular',
-    children: 'Typography/Body/Medium/regular',
+    variant: 'typography/body/medium/regular',
+    children: 'typography/body/medium/regular',
   },
 };
 
 // Body Small
 export const BodySmallBlack: Story = {
   args: {
-    variant: 'Typography/Body/Small/black',
-    children: 'Typography/Body/Small/black',
+    variant: 'typography/body/small/black',
+    children: 'typography/body/small/black',
   },
 };
 
 export const BodySmallBold: Story = {
   args: {
-    variant: 'Typography/Body/Small/bold',
-    children: 'Typography/Body/Small/bold',
+    variant: 'typography/body/small/bold',
+    children: 'typography/body/small/bold',
   },
 };
 
 export const BodySmallMedium: Story = {
   args: {
-    variant: 'Typography/Body/Small/medium',
-    children: 'Typography/Body/Small/medium',
+    variant: 'typography/body/small/medium',
+    children: 'typography/body/small/medium',
   },
 };
 
 export const BodySmallRegular: Story = {
   args: {
-    variant: 'Typography/Body/Small/regular',
-    children: 'Typography/Body/Small/regular',
+    variant: 'typography/body/small/regular',
+    children: 'typography/body/small/regular',
   },
 };
 
 // Label Large
 export const LabelLargeBlack: Story = {
   args: {
-    variant: 'Typography/Label/Large/black',
-    children: 'Typography/Label/Large/black',
+    variant: 'typography/label/large/black',
+    children: 'typography/label/large/black',
   },
 };
 
 export const LabelLargeBold: Story = {
   args: {
-    variant: 'Typography/Label/Large/bold',
-    children: 'Typography/Label/Large/bold',
+    variant: 'typography/label/large/bold',
+    children: 'typography/label/large/bold',
   },
 };
 
 export const LabelLargeMedium: Story = {
   args: {
-    variant: 'Typography/Label/Large/medium',
-    children: 'Typography/Label/Large/medium',
+    variant: 'typography/label/large/medium',
+    children: 'typography/label/large/medium',
   },
 };
 
 export const LabelLargeRegular: Story = {
   args: {
-    variant: 'Typography/Label/Large/regular',
-    children: 'Typography/Label/Large/regular',
+    variant: 'typography/label/large/regular',
+    children: 'typography/label/large/regular',
   },
 };
 
 // Label Medium
 export const LabelMediumBlack: Story = {
   args: {
-    variant: 'Typography/Label/Medium/black',
-    children: 'Typography/Label/Medium/black',
+    variant: 'typography/label/medium/black',
+    children: 'typography/label/medium/black',
   },
 };
 
 export const LabelMediumBold: Story = {
   args: {
-    variant: 'Typography/Label/Medium/bold',
-    children: 'Typography/Label/Medium/bold',
+    variant: 'typography/label/medium/bold',
+    children: 'typography/label/medium/bold',
   },
 };
 
 export const LabelMediumMedium: Story = {
   args: {
-    variant: 'Typography/Label/Medium/medium',
-    children: 'Typography/Label/Medium/medium',
+    variant: 'typography/label/medium/medium',
+    children: 'typography/label/medium/medium',
   },
 };
 
 export const LabelMediumRegular: Story = {
   args: {
-    variant: 'Typography/Label/Medium/regular',
-    children: 'Typography/Label/Medium/regular',
+    variant: 'typography/label/medium/regular',
+    children: 'typography/label/medium/regular',
   },
 };
 
 // Label Small
 export const LabelSmallBlack: Story = {
   args: {
-    variant: 'Typography/Label/Small/black',
-    children: 'Typography/Label/Small/black',
+    variant: 'typography/label/small/black',
+    children: 'typography/label/small/black',
   },
 };
 
 export const LabelSmallBold: Story = {
   args: {
-    variant: 'Typography/Label/Small/bold',
-    children: 'Typography/Label/Small/bold',
+    variant: 'typography/label/small/bold',
+    children: 'typography/label/small/bold',
   },
 };
 
 export const LabelSmallMedium: Story = {
   args: {
-    variant: 'Typography/Label/Small/medium',
-    children: 'Typography/Label/Small/medium',
+    variant: 'typography/label/small/medium',
+    children: 'typography/label/small/medium',
   },
 };
 
 export const LabelSmallRegular: Story = {
   args: {
-    variant: 'Typography/Label/Small/regular',
-    children: 'Typography/Label/Small/regular',
+    variant: 'typography/label/small/regular',
+    children: 'typography/label/small/regular',
   },
 };
