72f1b1c9596ee29263e985068653f2e782d7965d
flynn0909 <flynn@acon3d.com>
2024-03-12 14:05:34 +0900

    Typography type 수정

--- src/components/Typography/Typography.types.ts
+++ src/components/Typography/Typography.types.ts
@@ -1,8 +1,9 @@
-import { TypographyTypeMap } from '@mui/material/Typography/Typography';
+import { TypographyOwnProps } from '@mui/material/Typography/Typography';
 import { CustomTypographyVariants } from '@components/Typography/Typography.constants';
 import { CustomColorPaletteTypes } from '@shared/settings/color.type';
 
-export type TypographyProps = Pick<TypographyTypeMap['props'], 'children'> & {
+export type TypographyProps = {
+  children: TypographyOwnProps['children'];
   variant: CustomTypographyVariantsTypes;
   color?: CustomColorPaletteTypes;
 };
