646497e1db9d136d4f1434236d78549609913f78
flynn0909 <flynn@acon3d.com>
2024-03-12 17:23:12 +0900

    Icon 있는 Story 삭제
    - Controls id 오류

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react';
 import Button from './Button';
 import ThemeMui from '@shared/settings/ThemeMui.tsx';
-import { ChevronLeftIcon, ChevronRightIcon } from '@components';
 
 const meta: Meta<typeof Button> = {
   title: 'Component/Button',
@@ -18,51 +17,18 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-const commonArgs = {
-  children: 'button',
-  variant: 'contained',
-  size: 'L',
-  color: 'default',
-  disabled: false,
-  weight: 'regular',
-} as const;
-
-const excludedParameters = {
+export const Default: Story = {
+  args: {
+    children: 'button',
+    variant: 'contained',
+    size: 'L',
+    color: 'default',
+    disabled: false,
+    weight: 'regular',
+  },
   parameters: {
     controls: {
       exclude: ['startIcon', 'endIcon'],
     },
   },
-};
-
-export const Default: Story = {
-  args: {
-    ...commonArgs,
-  },
-  ...excludedParameters,
-};
-
-export const StartAndEnd: Story = {
-  args: {
-    ...commonArgs,
-    startIcon: <ChevronLeftIcon color={'color/green/500'} />,
-    endIcon: <ChevronRightIcon color={'color/green/500'} />,
-  },
-  ...excludedParameters,
-};
-
-export const Start: Story = {
-  args: {
-    ...commonArgs,
-    startIcon: <ChevronLeftIcon color={'color/green/500'} />,
-  },
-  ...excludedParameters,
-};
-
-export const End: Story = {
-  args: {
-    ...commonArgs,
-    endIcon: <ChevronRightIcon color={'color/green/500'} />,
-  },
-  ...excludedParameters,
 };
