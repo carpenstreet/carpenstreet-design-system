a713595fcb7320fab1ccd88fb96c63cfb472082c
flynn0909 <flynn@acon3d.com>
2024-03-15 18:02:25 +0900

    static asset에 대한 path 수정

--- .storybook/carpentheme.ts
+++ .storybook/carpentheme.ts
@@ -2,6 +2,6 @@ import { create } from '@storybook/theming/create';
 
 export default create({
   base: 'light',
-  brandImage: '../public/Carpenstreet_logo.png',
+  brandImage: '/Carpenstreet_logo.png',
   brandTarget: '_self',
 });
