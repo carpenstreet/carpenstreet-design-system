c9086a8342f15fc131521a57a8144ca7f2e2f797
add0215 <mingeon@acon3d.com>
2024-03-15 15:30:39 +0900

    Shadow 빌드 오류 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,17 +1,15 @@
 import Button from '@components/Button/Button';
 import Letter from '@components/Letter/Letter';
-import Shadow from '@components/Shadow/Shadow';
 import Icon from '@components/Icon/index';
 import Typography from '@components/Typography/Typography';
 import Grid from '@components/Grid/Grid';
 
-export { Button, Letter, Shadow, Typography, Grid, Icon };
+export { Button, Letter, Typography, Grid, Icon };
 export * from '@components/Icon/index';
 
 export default {
   Button,
   Letter,
-  Shadow,
   Typography,
   Grid,
   Icon,
