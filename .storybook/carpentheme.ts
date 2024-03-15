84aaca09c2d23fe651a9b81085f0f4a719a88c37
flynn0909 <flynn@acon3d.com>
2024-03-15 17:57:55 +0900

    favicon, brandImage 수정

--- .storybook/carpentheme.ts
+++ .storybook/carpentheme.ts
@@ -1,8 +1,7 @@
 import { create } from '@storybook/theming/create';
 
 export default create({
-  base: 'dark',
-  brandTitle: 'Carpenstreet Design System',
-  brandImage: 'https://carpenstreet.com/common/svg/logo.svg',
+  base: 'light',
+  brandImage: '../public/Carpenstreet_logo.png',
   brandTarget: '_self',
 });
