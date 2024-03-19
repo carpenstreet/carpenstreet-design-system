74488db741f65468f6acb8090118add38e51c44c
add0215 <mingeon@acon3d.com>
2024-03-19 23:00:27 +0900

    Updates

---
+++ assets/ArrowDownIcon.stories-ad5c0aa7.js
@@ -0,0 +1,7 @@
+import{j as r}from"./jsx-runtime-29545a09.js";import{T as s}from"./ThemeMui-12e5628f.js";import{A as c}from"./ArrowDownIcon-4865cef5.js";const i={title:"Foundation/Icon/ArrowDown",component:c,decorators:[n=>r(s,{children:r(n,{})})]},o={args:{width:24,height:24,color:"color/gray/800"}};var e,t,a;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    width: 24,
+    height: 24,
+    color: 'color/gray/800'
+  }
+}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const l=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:o,__namedExportsOrder:l,default:i},Symbol.toStringTag,{value:"Module"}));export{u as A,o as D};
--- assets/RemoveIcon.stories-83d7b979.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./jsx-runtime-29545a09.js";import{T as n}from"./ThemeMui-d55e327a.js";import{R as c}from"./RemoveIcon-e7ad5a2d.js";const m={title:"Foundation/Icon/Remove",component:c,decorators:[s=>e(n,{children:e(s,{})})]},o={args:{width:24,height:24,color:"color/gray/800"}};var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    width: 24,
-    height: 24,
-    color: 'color/gray/800'
-  }
-}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["Default"],u=Object.freeze(Object.defineProperty({__proto__:null,Default:o,__namedExportsOrder:i,default:m},Symbol.toStringTag,{value:"Module"}));export{o as D,u as R};
