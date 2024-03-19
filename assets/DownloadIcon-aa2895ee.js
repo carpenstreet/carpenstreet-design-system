f3f6a3ee10cabe3d7e57f54e54571c5217ec7ccb
add0215 <mingeon@acon3d.com>
2024-03-19 21:47:23 +0900

    Updates

--- assets/DownloadIcon-aa2895ee.js
+++
@@ -1,8 +0,0 @@
-import{j as o,a as i,F as a}from"./jsx-runtime-29545a09.js";import{M as m,C as s,b as c}from"./index-36515edb.js";import{D as l,a as p}from"./DownloadIcon.stories-97d2d027.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-7a45b377.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./inheritsLoose-8349f581.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-d55e327a.js";import"./color-d4584655.js";import"./DownloadIcon-ec6a10d7.js";import"./useTheme-d5e09c70.js";function r(n){const t=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),n.components);return i(a,{children:[o(m,{of:l}),`
-`,o(t.h1,{id:"download-icon",children:"Download Icon"}),`
-`,i(t.ul,{children:[`
-`,o(t.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
-`,o(t.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
-`]}),`
-`,o(s,{of:p}),`
-`,o(c,{})]})}function y(n={}){const{wrapper:t}=Object.assign({},e(),n.components);return t?o(t,Object.assign({},n,{children:o(r,n)})):r(n)}export{y as default};
---
+++ assets/Grid.stories-48b5ea85.js
@@ -0,0 +1,16 @@
+import{j as e}from"./jsx-runtime-29545a09.js";import{R as s}from"./index-76fb7be0.js";import{G as i}from"./Grid-0df5fe6f.js";import{T as m}from"./ThemeMui-d55e327a.js";import{P as l}from"./Paper-cd604431.js";const c={title:"Foundation/Grid",component:i,decorators:[o=>e(m,{children:e(o,{})})]},r={args:{direction:"row",container:!0,sx:{},children:e(s.Fragment,{children:new Array(12).fill(null).map((o,d)=>e(i,{item:!0,mobile:1,desktop:1,children:e(l,{elevation:0,sx:{height:"100px",backgroundColor:"#FFE5E5",padding:0}})},d))})}};var n,t,a;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  args: {
+    direction: 'row',
+    container: true,
+    sx: {},
+    children: <React.Fragment>
+        {new Array(12).fill(null).map((_, idx) => <Grid item key={idx} mobile={1} desktop={1}>
+            <Paper elevation={0} sx={{
+          height: '100px',
+          backgroundColor: '#FFE5E5',
+          padding: 0
+        }} />
+          </Grid>)}
+      </React.Fragment>
+  }
+}`,...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const p=["GridExample"],G=Object.freeze(Object.defineProperty({__proto__:null,GridExample:r,__namedExportsOrder:p,default:c},Symbol.toStringTag,{value:"Module"}));export{G,r as a};
