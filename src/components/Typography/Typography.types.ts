3ae69023f7d4c1f32eda0f47e62ec3c3f3ad7e2c
flynn0909 <flynn@acon3d.com>
2024-03-19 14:38:19 +0900

    Typography props type 수정

--- src/components/Typography/Typography.types.ts
+++ src/components/Typography/Typography.types.ts
@@ -2,7 +2,7 @@ import { TypographyProps as MUITypographyProps } from '@mui/material/Typography/
 import { CustomTypographyVariants } from '@components/Typography/Typography.constants';
 import { CustomColorPaletteTypes } from '@shared/settings/color/color.type.ts';
 
-export type TypographyProps = {
+export type TypographyProps = Omit<MUITypographyProps, 'children' | 'variant' | 'color'> & {
   children: MUITypographyProps['children'];
   variant: CustomTypographyVariantsTypes;
   color?: CustomColorPaletteTypes;
