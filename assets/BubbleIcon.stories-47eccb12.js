de2db69fa76d5f487e7228d09bc73196e77b2a50
flynn0909 <flynn@acon3d.com>
2024-03-19 16:24:08 +0900

    Updates

--- assets/BubbleIcon.stories-47eccb12.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./jsx-runtime-29545a09.js";import{T as n}from"./ThemeMui-d55e327a.js";import{B as c}from"./BubbleIcon-d572c9e9.js";const l={title:"Foundation/Icon/Bubble",component:c,decorators:[s=>e(n,{children:e(s,{})})]},o={args:{width:24,height:24,color:"color/gray/800"}};var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    width: 24,
-    height: 24,
-    color: 'color/gray/800'
-  }
-}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const i=["Default"],p=Object.freeze(Object.defineProperty({__proto__:null,Default:o,__namedExportsOrder:i,default:l},Symbol.toStringTag,{value:"Module"}));export{p as B,o as D};
--- assets/ProfileFilledIcon-40bb5933.js
+++
@@ -1,8 +0,0 @@
-import{j as o,a as n,F as m}from"./jsx-runtime-29545a09.js";import{M as l,C as s,b as a}from"./index-9843d489.js";import{P as c,D as p}from"./ProfileFilledIcon.stories-7d539e40.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-812d3628.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./inheritsLoose-8349f581.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-d55e327a.js";import"./color-d4584655.js";import"./ProfileFilledIcon-4b5ff417.js";import"./useTheme-d5e09c70.js";function r(i){const t=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),i.components);return n(m,{children:[o(l,{of:c}),`
-`,o(t.h1,{id:"profile-filled-icon",children:"Profile Filled Icon"}),`
-`,n(t.ul,{children:[`
-`,o(t.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
-`,o(t.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
-`]}),`
-`,o(s,{of:p}),`
-`,o(a,{})]})}function v(i={}){const{wrapper:t}=Object.assign({},e(),i.components);return t?o(t,Object.assign({},i,{children:o(r,i)})):r(i)}export{v as default};
