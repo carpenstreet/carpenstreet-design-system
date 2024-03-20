e6a63992232eb40f737454c662e6fd0049c935df
flynn0909 <flynn@acon3d.com>
2024-03-20 15:20:38 +0900

    테스트 rollback

--- rollup.config.js
+++ rollup.config.js
@@ -2,78 +2,96 @@ import tsconfigPaths from 'rollup-plugin-tsconfig-paths';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
-import dts from 'rollup-plugin-dts';
+import dts from 'rollup-plugin-dts'
+import modify from 'rollup-plugin-modify';
 import { terser } from 'rollup-plugin-terser';
-import commonjs from '@rollup/plugin-commonjs';
 
-const extensions = ['js', 'jsx', 'ts', 'tsx', 'mjs'];
+const extensions = [ 'js', 'jsx', 'ts', 'tsx', 'mjs' ];
 
 const minifierPlugin = terser({
-  compress: {
-    passes: 10,
-    keep_infinity: true,
-    pure_getters: true,
-  },
-  format: {
-    wrap_func_args: false,
-    preserve_annotations: true,
-  },
+    compress: {
+        passes: 10,
+        keep_infinity: true,
+        pure_getters: true,
+    },
+    format: {
+        wrap_func_args: false,
+        preserve_annotations: true,
+    },
 });
 
 const config = [
-  {
-    external: [/node_modules/],
-    input: './src/index.ts',
-    output: [
-      {
-        dir: './dist/',
-        format: 'esm',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
-    ],
-    plugins: [tsconfigPaths(), nodeResolve({ extensions }), peerDepsExternal(), esbuild(), minifierPlugin, commonjs()],
-  },
-  {
-    external: [/node_modules/],
-    input: './src/index.ts',
-    output: [
-      {
-        dir: './dist/',
-        format: 'esm',
-        // preserveModules: true,
-        // preserveModulesRoot: 'src'
-      },
-    ],
-    plugins: [tsconfigPaths(), nodeResolve({ extensions }), peerDepsExternal(), esbuild(), minifierPlugin, commonjs()],
-  },
-  {
-    external: [/node_modules/],
-    input: './src/index.ts',
-    output: [
-      {
-        dir: './dist',
-        entryFileNames: '[name].d.ts',
-        format: 'esm',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
-    ],
-    plugins: [tsconfigPaths(), dts({ respectExternal: false }), commonjs()],
-  },
-  {
-    external: [/node_modules/],
-    input: './src/index.ts',
-    output: [
-      {
-        dir: './dist',
-        entryFileNames: '[name].d.ts',
-        format: 'esm',
-        // preserveModules: true,
-        // preserveModulesRoot: 'src'
-      },
-    ],
-    plugins: [tsconfigPaths(), dts({ respectExternal: false }), commonjs()],
-  },
+    {
+        external: [ /node_modules/ ],
+        input: './src/index.ts',
+        output: [
+            {
+                dir: './dist/',
+                format: 'esm',
+                preserveModules: true,
+                preserveModulesRoot: 'src'
+            },
+        ],
+        plugins: [
+            tsconfigPaths(),
+            nodeResolve({ extensions }),
+            peerDepsExternal(),
+            esbuild(),
+            minifierPlugin,
+        ]
+    },
+    {
+        external: [ /node_modules/ ],
+        input: './src/index.ts',
+        output: [
+            {
+                dir: './dist/',
+                format: 'esm',
+                // preserveModules: true,
+                // preserveModulesRoot: 'src'
+            },
+        ],
+        plugins: [
+            tsconfigPaths(),
+            nodeResolve({ extensions }),
+            peerDepsExternal(),
+            esbuild(),
+            minifierPlugin,
+        ]
+    },
+    {
+        external: [ /node_modules/ ],
+        input: './src/index.ts',
+        output: [
+            {
+                dir: './dist',
+                entryFileNames: '[name].d.ts',
+                format: 'esm',
+                preserveModules: true,
+                preserveModulesRoot: 'src'
+            },
+        ],
+        plugins: [
+            tsconfigPaths(),
+            dts({respectExternal: false}),
+        ]
+    },
+    {
+        external: [ /node_modules/ ],
+        input: './src/index.ts',
+        output: [
+            {
+                dir: './dist',
+                entryFileNames: '[name].d.ts',
+                format: 'esm',
+                // preserveModules: true,
+                // preserveModulesRoot: 'src'
+            },
+        ],
+        plugins: [
+            tsconfigPaths(),
+            dts({respectExternal: false}),
+        ]
+    },
 ];
 export default config;
