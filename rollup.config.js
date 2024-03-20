5ef8a2aaaf9f0d03b559b69fe665e92eb87e1ce4
flynn0909 <flynn@acon3d.com>
2024-03-20 18:14:54 +0900

    config 수정 테스트 (script 수정 및 config 수정)

--- rollup.config.js
+++ rollup.config.js
@@ -2,18 +2,12 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import preserveDirectives from 'rollup-preserve-directives';
 import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const config = [
   {
+    // external: [ /node_modules/ ],
     input: 'src/index.ts',
     output: [
-      {
-        dir: './dist/',
-        format: 'cjs',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-      },
       {
         dir: './dist/',
         format: 'esm',
@@ -21,7 +15,7 @@ const config = [
         preserveModulesRoot: 'src',
       },
     ],
-    plugins: [peerDepsExternal(), typescript({ compilerOptions: { declaration: true, declarationDir: './dist/types' } }), nodeResolve(), preserveDirectives(), commonjs()],
+    plugins: [typescript({ compilerOptions: { declaration: true, declarationDir: './dist/types' } }), nodeResolve({}), preserveDirectives(), commonjs()],
   },
 ];
 export default config;
