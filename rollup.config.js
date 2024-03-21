896d8c8881ee3c63dc07a0c1fc765cd98beeb2e2
flynn0909 <flynn@acon3d.com>
2024-03-21 10:53:10 +0900

    bundling test: declarationDir 수정

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import preserveDirectives from 'rollup-preserve-directives';
+
+// import preserveDirectives from 'rollup-preserve-directives';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
@@ -11,9 +12,21 @@ const config = [
       {
         dir: './dist/',
         format: 'esm',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
       },
     ],
-    plugins: [peerDepsExternal(), nodeResolve(), commonjs(), typescript({ compilerOptions: { declaration: true, declarationDir: './dist/types' } }), preserveDirectives()],
+    plugins: [
+      peerDepsExternal(),
+      nodeResolve(),
+      commonjs({ include: /node_modules/ }),
+      typescript({
+        compilerOptions: {
+          declaration: true,
+          declarationDir: './dist',
+        },
+      }),
+    ],
   },
 ];
 export default config;
