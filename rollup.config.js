01e550047bac8a706122d11148d2a5b927e0a5e2
flynn0909 <flynn@acon3d.com>
2024-03-21 15:43:53 +0900

    Rollup config에 주석 추가

--- rollup.config.js
+++ rollup.config.js
@@ -14,11 +14,17 @@ const config = [
         format: 'esm',
       },
     ],
-    plugins: [peerDepsExternal(), nodeResolve(), commonjs(), typescript(), preserveDirectives()],
+    plugins: [
+      peerDepsExternal(), // peer dependency를 external로 설정해주는 plugin
+      nodeResolve(), // node_modules에서 모듈을 불러오는데 필요한 plugin
+      commonjs(), // CommonJS 모듈을 ES로 변환해주는 plugin
+      typescript(), // typescript를 컴파일해주는 plugin
+      preserveDirectives(), // 'use client' 등의 지시문을 유지해주는 plugin
+    ],
   },
   {
-    input: './dist/src/index.d.ts',
-    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    input: './dist/src/index.d.ts', // 위 config에서 생성된 ./dist/src/index.d.ts 파일을 input으로 설정
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }], // ./dist/index.d.ts를 생성
     plugins: [dts()],
   },
 ];
