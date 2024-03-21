60d43aa76e5ac39d2a2c2facce437fb69ab6b732
flynn0909 <flynn@acon3d.com>
2024-03-21 13:55:09 +0900

    v1.1.25~27 rollback

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,19 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-
-// import preserveDirectives from 'rollup-preserve-directives';
+import preserveDirectives from 'rollup-preserve-directives';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-// import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
 
 const config = [
   {
-    external: [/node_modules/],
     input: 'src/index.ts',
     output: [
       {
         dir: './dist/',
         format: 'esm',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
       },
     ],
-    plugins: [
-      peerDepsExternal(),
-      nodeResolve(),
-      commonjs(),
-      typescript({
-        compilerOptions: {
-          declaration: false,
-        },
-      }),
-    ],
+    plugins: [peerDepsExternal(), nodeResolve(), commonjs(), typescript({ compilerOptions: { declaration: true, declarationDir: './dist/types' } }), preserveDirectives()],
   },
 ];
 export default config;
