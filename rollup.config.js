f969248d2c3addbdbfa108550bd65c2dc134ae97
flynn0909 <flynn@acon3d.com>
2024-03-21 13:36:27 +0900

    bundling test: node_moudles 경로 못 찾는 문제 테스트

--- rollup.config.js
+++ rollup.config.js
@@ -4,26 +4,27 @@ import typescript from '@rollup/plugin-typescript';
 // import preserveDirectives from 'rollup-preserve-directives';
 import commonjs from '@rollup/plugin-commonjs';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+// import tsConfigPaths from 'rollup-plugin-tsconfig-paths';
 
 const config = [
   {
+    external: [/node_modules/],
     input: 'src/index.ts',
     output: [
       {
         dir: './dist/',
         format: 'esm',
         preserveModules: true,
-        // preserveModulesRoot: 'src',
+        preserveModulesRoot: 'src',
       },
     ],
     plugins: [
       peerDepsExternal(),
       nodeResolve(),
-      commonjs({ include: /node_modules/ }),
+      commonjs(),
       typescript({
         compilerOptions: {
-          declaration: true,
-          declarationDir: './dist',
+          declaration: false,
         },
       }),
     ],
