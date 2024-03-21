e3ed13427a60eceb71800d94b3a2751bb9c70134
flynn0909 <flynn@acon3d.com>
2024-03-21 16:36:18 +0900

    icon을 이젠 다 따로따로 import함
    - rollup warning 떠서 이렇게 변경함

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,82 @@
 import Button from './Button/Button';
 import IconButton from './IconButton/IconButton';
-import Icon from './Icon/index';
+import ProfileFilledIcon from './Icon/ProfileFilled/ProfileFilledIcon';
+import SearchIcon from './Icon/Search/SearchIcon';
+import CategoryIcon from './Icon/Cateogry/CategoryIcon';
+import StarIcon from './Icon/Star/StarIcon';
+import ImageIcon from './Icon/Image/ImageIcon';
+import DownloadIcon from './Icon/Download/DownloadIcon';
+import MinusIcon from './Icon/Minus/MinusIcon';
+import CloseIcon from './Icon/Close/CloseIcon';
+import VVIPIcon from './Icon/Vvip/VVIPIcon';
+import FilterIcon from './Icon/Filter/FilterIcon';
+import ViewIcon from './Icon/View/ViewIcon';
+import CopyIcon from './Icon/Copy/CopyIcon';
+import WritingIcon from './Icon/Writing/WritingIcon';
+import BoxIcon from './Icon/Box/BoxIcon';
+import EarningIcon from './Icon/Earning/EarningIcon';
+import NoticeOutlineIcon from './Icon/NoticeOutline/NoticeOutlineIcon';
+import NoticeIcon from './Icon/Notice/NoticeIcon';
+import InformationOutlineIcon from './Icon/InformationOutline/InformationOutlineIcon';
+import InformationIcon from './Icon/Information/InformationIcon';
+import RemoveIcon from './Icon/Remove/RemoveIcon';
+import ChevronLeftIcon from './Icon/ChevronLeft/ChevronLeftIcon';
+import ChevronRightIcon from './Icon/ChevronRight/ChevronRightIcon';
+import ChevronDownIcon from './Icon/ChevronDown/ChevronDownIcon';
+import ChevronUpIcon from './Icon/ChevronUp/ChevronUpIcon';
+import CaretUpIcon from './Icon/CaretUp/CaretUpIcon';
+import CaretDownIcon from './Icon/CaretDown/CaretDownIcon';
+import ArrowDownIcon from './Icon/ArrowDown/ArrowDownIcon';
+import ArrowUpIcon from './Icon/ArrowUp/ArrowUpIcon';
+import ArrowRightIcon from './Icon/ArrowRight/ArrowRightIcon';
+import ArrowLeftIcon from './Icon/ArrowLeft/ArrowLeftIcon';
+import SecurityIcon from './Icon/Security/SecurityIcon';
+import BubbleIcon from './Icon/Bubble/BubbleIcon';
 import Typography from './Typography/Typography';
 import Grid from './Grid/Grid';
 import Checkbox from './Checkbox/Checkbox';
 import Tooltip from './Tooltip/Tooltip';
 
-export { Button, IconButton, Typography, Grid, Icon, Checkbox, Tooltip };
+export {
+  Button,
+  IconButton,
+  Typography,
+  Grid,
+  ProfileFilledIcon,
+  SearchIcon,
+  CategoryIcon,
+  StarIcon,
+  ImageIcon,
+  DownloadIcon,
+  MinusIcon,
+  CloseIcon,
+  VVIPIcon,
+  FilterIcon,
+  ViewIcon,
+  CopyIcon,
+  WritingIcon,
+  BoxIcon,
+  EarningIcon,
+  NoticeOutlineIcon,
+  NoticeIcon,
+  InformationOutlineIcon,
+  InformationIcon,
+  RemoveIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  CaretUpIcon,
+  CaretDownIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  SecurityIcon,
+  BubbleIcon,
+  Checkbox,
+  Tooltip,
+};
 export * from './Icon/index';
 
 export default {
@@ -14,8 +84,38 @@ export default {
   IconButton,
   Typography,
   Grid,
-  Icon,
-  ...Icon,
+  ProfileFilledIcon,
+  SearchIcon,
+  CategoryIcon,
+  StarIcon,
+  ImageIcon,
+  DownloadIcon,
+  MinusIcon,
+  CloseIcon,
+  VVIPIcon,
+  FilterIcon,
+  ViewIcon,
+  CopyIcon,
+  WritingIcon,
+  BoxIcon,
+  EarningIcon,
+  NoticeOutlineIcon,
+  NoticeIcon,
+  InformationOutlineIcon,
+  InformationIcon,
+  RemoveIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+  CaretUpIcon,
+  CaretDownIcon,
+  ArrowDownIcon,
+  ArrowUpIcon,
+  ArrowRightIcon,
+  ArrowLeftIcon,
+  SecurityIcon,
+  BubbleIcon,
   Checkbox,
   Tooltip,
 };
