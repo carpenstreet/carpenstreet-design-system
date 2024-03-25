2ed399453a246fe6ff10a1362a74dcf56fe70cf9
flynn0909 <flynn@acon3d.com>
2024-03-25 14:38:18 +0900

    Label 작업 완료

--- src/components/Label/Label.stories.tsx
+++ src/components/Label/Label.stories.tsx
@@ -19,7 +19,169 @@ type Story = StoryObj<typeof Label>;
 
 export const Default: Story = {
   args: {
+    children: 'Label',
+    variant: 'filled',
+    color: 'default',
+    sx: {},
+  },
+};
+
+export const FilledDefault: Story = {
+  args: {
+    children: 'Default',
+    variant: 'filled',
+    color: 'default',
+  },
+};
+
+export const FilledPrimary: Story = {
+  args: {
     children: 'Primary',
+    variant: 'filled',
     color: 'primary',
+  },
+};
+
+export const FilledInfo: Story = {
+  args: {
+    children: 'Info',
+    variant: 'filled',
+    color: 'info',
+  },
+};
+
+export const FilledSuccess: Story = {
+  args: {
+    children: 'Success',
+    variant: 'filled',
+    color: 'success',
+  },
+};
+
+export const FilledError: Story = {
+  args: {
+    children: 'Error',
+    variant: 'filled',
+    color: 'error',
+  },
+};
+
+export const SoftDefault: Story = {
+  args: {
+    children: 'Default',
+    variant: 'soft',
+    color: 'default',
+  },
+};
+
+export const SoftPrimary: Story = {
+  args: {
+    children: 'Primary',
+    variant: 'soft',
+    color: 'primary',
+  },
+};
+
+export const SoftInfo: Story = {
+  args: {
+    children: 'Info',
+    variant: 'soft',
+    color: 'info',
+  },
+};
+
+export const SoftSuccess: Story = {
+  args: {
+    children: 'Success',
+    variant: 'soft',
+    color: 'success',
+  },
+};
+
+export const SoftError: Story = {
+  args: {
+    children: 'Error',
+    variant: 'soft',
+    color: 'error',
+  },
+};
+
+export const OutlinedDefault: Story = {
+  args: {
+    children: 'Default',
+    variant: 'outlined',
+    color: 'default',
+  },
+};
+
+export const OutlinedPrimary: Story = {
+  args: {
+    children: 'Primary',
+    variant: 'outlined',
+    color: 'primary',
+  },
+};
+
+export const OutlinedInfo: Story = {
+  args: {
+    children: 'Info',
+    variant: 'outlined',
+    color: 'info',
+  },
+};
+
+export const OutlinedSuccess: Story = {
+  args: {
+    children: 'Success',
+    variant: 'outlined',
+    color: 'success',
+  },
+};
+
+export const OutlinedError: Story = {
+  args: {
+    children: 'Error',
+    variant: 'outlined',
+    color: 'error',
+  },
+};
+
+export const SmallDefault: Story = {
+  args: {
+    children: 'N',
+    variant: 'small',
+    color: 'default',
+  },
+};
+
+export const SmallPrimary: Story = {
+  args: {
+    children: 'N',
+    variant: 'small',
+    color: 'primary',
+  },
+};
+
+export const SmallInfo: Story = {
+  args: {
+    children: 'N',
+    variant: 'small',
+    color: 'info',
+  },
+};
+
+export const SmallSuccess: Story = {
+  args: {
+    children: 'N',
+    variant: 'small',
+    color: 'success',
+  },
+};
+
+export const SmallError: Story = {
+  args: {
+    children: 'N',
+    variant: 'small',
+    color: 'error',
   },
 };
--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -4,25 +4,101 @@ import { useTheme } from '@mui/material/styles';
 import Typography from '../Typography/Typography.tsx';
 
 export default function Label(props: LabelProps) {
-  const { children, color, ...rest } = props;
+  const { children, color = 'default', variant, sx } = props;
+
   const theme = useTheme();
-  const layout = {};
+
+  const boxStyle = (() => {
+    const commonStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: 'fit-content',
+      height: '22px',
+      padding: '4px 8px',
+      borderRadius: '4px',
+    };
+    if (variant === 'filled') {
+      if (color === 'default')
+        return {
+          ...commonStyle,
+          backgroundColor: theme.palette['color/gray/600'],
+        };
+      if (color === 'primary') return { ...commonStyle, backgroundColor: theme.palette['color/primary/600'] };
+      if (color === 'info') return { ...commonStyle, backgroundColor: theme.palette['color/state/info'] };
+      if (color === 'success') return { ...commonStyle, backgroundColor: theme.palette['color/state/success'] };
+      if (color === 'error') return { ...commonStyle, backgroundColor: theme.palette['color/state/error'] };
+    }
+
+    if (variant === 'soft') {
+      if (color === 'default')
+        return {
+          ...commonStyle,
+          backgroundColor: theme.palette['color/gray/100'],
+        };
+      if (color === 'primary') return { ...commonStyle, backgroundColor: theme.palette['color/primary/dim/300'] };
+      if (color === 'info') return { ...commonStyle, backgroundColor: theme.palette['color/state/infoDim/300'] };
+      if (color === 'success') return { ...commonStyle, backgroundColor: theme.palette['color/state/successDim/300'] };
+      if (color === 'error') return { ...commonStyle, backgroundColor: theme.palette['color/state/errorDim/300'] };
+    }
+
+    if (variant === 'outlined') {
+      if (color === 'default')
+        return {
+          ...commonStyle,
+          backgroundColor: 'transparent',
+          boxShadow: `0 0 0 1px ${theme.palette['color/gray/600']} inset`,
+        };
+      if (color === 'primary') return { ...commonStyle, backgroundColor: 'transparent', boxShadow: `0 0 0 1px ${theme.palette['color/primary/600']} inset` };
+      if (color === 'info') return { ...commonStyle, backgroundColor: 'transparent', boxShadow: `0 0 0 1px ${theme.palette['color/state/info']} inset` };
+      if (color === 'success') return { ...commonStyle, backgroundColor: 'transparent', boxShadow: `0 0 0 1px ${theme.palette['color/state/success']} inset` };
+      if (color === 'error') return { ...commonStyle, backgroundColor: 'transparent', boxShadow: `0 0 0 1px ${theme.palette['color/state/error']} inset` };
+    }
+    if (variant === 'small') {
+      if (color === 'default')
+        return {
+          ...commonStyle,
+          width: '16px',
+          height: '16px',
+          borderRadius: '100px',
+          backgroundColor: theme.palette['color/gray/100'],
+        };
+      if (color === 'primary') return { ...commonStyle, width: '16px', height: '16px', borderRadius: '100px', backgroundColor: theme.palette['color/primary/600'] };
+      if (color === 'info') return { ...commonStyle, width: '16px', height: '16px', borderRadius: '100px', backgroundColor: theme.palette['color/state/info'] };
+      if (color === 'success') return { ...commonStyle, width: '16px', height: '16px', borderRadius: '100px', backgroundColor: theme.palette['color/state/success'] };
+      if (color === 'error') return { ...commonStyle, width: '16px', height: '16px', borderRadius: '100px', backgroundColor: theme.palette['color/state/error'] };
+    }
+  })();
+
+  const boxSx = { ...boxStyle, ...sx };
+
+  const typoVariant = variant === 'small' ? 'typography/label/small/bold' : 'typography/label/medium/bold';
+
+  const typoColor = (() => {
+    if (variant === 'filled') return 'color/white';
+    if (variant === 'soft') {
+      if (color === 'default') return 'color/gray/800';
+      if (color === 'primary') return 'color/primary/600';
+      if (color === 'info') return 'color/state/info';
+      if (color === 'success') return 'color/state/success';
+      if (color === 'error') return 'color/state/error';
+    }
+    if (variant === 'outlined') {
+      if (color === 'default') return 'color/gray/600';
+      if (color === 'primary') return 'color/primary/600';
+      if (color === 'info') return 'color/state/info';
+      if (color === 'success') return 'color/state/success';
+      if (color === 'error') return 'color/state/error';
+    }
+    if (variant === 'small') return color === 'default' ? 'color/gray/800' : 'color/white';
+  })();
+
+  const childrenToRender = variant === 'small' && typeof children === 'string' ? children.slice(0, 1) : children;
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 'fit-content',
-        height: '22px',
-        padding: '4px 8px',
-        borderRadius: '4px',
-        backgroundColor: theme.palette['color/primary/600'],
-        color: theme.palette['color/white'],
-      }}
-    >
-      <Typography variant={'typography/label/medium/bold'} color={'color/white'}>
-        {children}
+    <Box sx={boxSx}>
+      <Typography variant={typoVariant} color={typoColor}>
+        {childrenToRender}
       </Typography>
     </Box>
   );
