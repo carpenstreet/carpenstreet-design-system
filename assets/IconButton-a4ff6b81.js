0da842fdd08d37094610b96d62c803092904ed49
add0215 <135012363+mingeon0215@users.noreply.github.com>
2024-03-25 13:08:27 +0900

    Updates

---
+++ assets/IconButton-a4ff6b81.js
@@ -0,0 +1,16 @@
+import{j as o,a as r,F as s}from"./jsx-runtime-29545a09.js";import{M as m,C as i,b as d}from"./index-f995962f.js";import{I as p,D as c,a as l,b as h,P as f,c as u,d as b,e as D}from"./IconButton.stories-19f6cac3.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-5cff7bca.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-6a2b30a9.js";import"./color-d4584655.js";import"./generateUtilityClasses-ea45776e.js";import"./styled-c7a0d81f.js";import"./ButtonBase-de37006e.js";import"./TransitionGroupContext-1a193ae6.js";import"./CategoryIcon-54c17826.js";import"./useTheme-4fe578df.js";function e(n){const t=Object.assign({h1:"h1",p:"p",code:"code",h2:"h2",h3:"h3"},a(),n.components);return r(s,{children:[o(m,{of:p}),`
+`,o(t.h1,{id:"iconbutton",children:"IconButton"}),`
+`,r(t.p,{children:["ACON3D 서비스에 사용되는 아이콘 버튼입니다. 기본적으로 정해진 preset을 사용하되, 필요에 따라 ",o(t.code,{children:"sx"})," prop을 통해 스타일을 커스텀할 수 있습니다."]}),`
+`,o(t.p,{children:"버튼 내부의 아이콘은 미리 정해진 preset에 따라 색상과 크기가 자동으로 변경됩니다."}),`
+`,o(i,{of:c}),`
+`,o(d,{include:["color","size","disabled","sx"]}),`
+`,o(t.h2,{id:"variants",children:"Variants"}),`
+`,o(t.h3,{id:"default",children:"default"}),`
+`,o(i,{of:l}),`
+`,o(i,{of:h}),`
+`,o(t.h3,{id:"primary",children:"primary"}),`
+`,o(i,{of:f}),`
+`,o(i,{of:u}),`
+`,o(t.h3,{id:"disabled",children:"disabled"}),`
+`,o(i,{of:b}),`
+`,o(i,{of:D})]})}function E(n={}){const{wrapper:t}=Object.assign({},a(),n.components);return t?o(t,Object.assign({},n,{children:o(e,n)})):e(n)}export{E as default};
