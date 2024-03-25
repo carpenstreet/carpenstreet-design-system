1e27f96db5ca72790ada6ab8c1fa6b774b36dfab
flynn0909 <flynn@acon3d.com>
2024-03-25 14:56:10 +0900

    vvip icon 오류 수정

--- src/components/Icon/Vvip/VVIPIcon.tsx
+++ src/components/Icon/Vvip/VVIPIcon.tsx
@@ -8,7 +8,7 @@ export default function VVIPIcon({ width = 24, height = 24, color = 'color/gray/
 
   return (
     <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.63 12.37L12 8.5L8.37 12.37L4.5 8.5V16.5H11.5H12.5H19.5V8.5L15.63 12.37Z" fill="#313135" stroke={iconColor} strokeLinecap="round" strokeLinejoin="round" />
+      <path d="M15.63 12.37L12 8.5L8.37 12.37L4.5 8.5V16.5H11.5H12.5H19.5V8.5L15.63 12.37Z" fill={iconColor} stroke={iconColor} strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   );
 }
