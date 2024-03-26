e5957810ef8f7e6739e336fdf4dae38a1ae5c41f
add0215 <mingeon@acon3d.com>
2024-03-26 14:51:26 +0900

    Updates

---
+++ assets/ArrowUpIcon-8193c946.js
@@ -0,0 +1,8 @@
+import{j as o,a as n,F as m}from"./jsx-runtime-29545a09.js";import{M as s,C as a,b as p}from"./index-1f44bf12.js";import{A as c,D as l}from"./ArrowUpIcon.stories-99a3bd49.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-af8c7a29.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-9e5ceceb.js";import"./color-d4584655.js";import"./ArrowUpIcon-bad07ecb.js";import"./useTheme-256a493e.js";function i(r){const t=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),r.components);return n(m,{children:[o(s,{of:c}),`
+`,o(t.h1,{id:"arrowup-icon",children:"ArrowUp Icon"}),`
+`,n(t.ul,{children:[`
+`,o(t.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
+`,o(t.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
+`]}),`
+`,o(a,{of:l}),`
+`,o(p,{})]})}function v(r={}){const{wrapper:t}=Object.assign({},e(),r.components);return t?o(t,Object.assign({},r,{children:o(i,r)})):i(r)}export{v as default};
