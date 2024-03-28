011620c2460c3131ef9c1633f1e02fca9cbe3e35
flynn0909 <124758326+flynn0909@users.noreply.github.com>
2024-03-28 14:07:13 +0900

    icon 일부 svg 수정 (#44)

--- src/components/Icon/ProfileFilled/ProfileFilledIcon.tsx
+++ src/components/Icon/ProfileFilled/ProfileFilledIcon.tsx
@@ -8,8 +8,18 @@ export default function ProfileFilledIcon({ width = 24, height = 24, color = 'co
   return (
     <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <g clipPath="url(#clip0_4337_1980)">
-        <circle cx="12" cy="7" r="3" fill={iconColor} stroke={iconColor} strokeWidth="2" />
-        <path d="M12 14C7.58172 14 4 16.6863 4 20H20C20 16.6863 16.4183 14 12 14Z" fill={iconColor} stroke={iconColor} strokeWidth="2" strokeLinejoin="round" />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7Z"
+          fill={iconColor}
+        />
+        <path
+          fillRule="evenodd"
+          clipRule="evenodd"
+          d="M5.74315 14.9574C7.37998 13.7297 9.59541 13 12 13C14.4046 13 16.62 13.7297 18.2569 14.9574C19.894 16.1852 21 17.9578 21 20C21 20.5523 20.5523 21 20 21H4C3.44772 21 3 20.5523 3 20C3 17.9578 4.10601 16.1852 5.74315 14.9574Z"
+          fill={iconColor}
+        />
       </g>
       <defs>
         <clipPath id="clip0_4337_1980">
--- src/components/Icon/Vvip/VVIPIcon.tsx
+++ src/components/Icon/Vvip/VVIPIcon.tsx
@@ -8,7 +8,12 @@ export default function VVIPIcon({ width = 24, height = 24, color = 'color/gray/
 
   return (
     <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.63 12.37L12 8.5L8.37 12.37L4.5 8.5V16.5H11.5H12.5H19.5V8.5L15.63 12.37Z" fill={iconColor} stroke={iconColor} strokeLinecap="round" strokeLinejoin="round" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M4.30866 8.03807C4.4955 7.96068 4.71055 8.00346 4.85355 8.14646L8.35851 11.6514L11.6353 8.15795C11.7298 8.05718 11.8618 8.00001 12 8.00001C12.1382 8.00001 12.2702 8.05718 12.3647 8.15795L15.6415 11.6514L19.1464 8.14646C19.2894 8.00346 19.5045 7.96068 19.6913 8.03807C19.8782 8.11547 20 8.29778 20 8.50001V16.5C20 16.7762 19.7761 17 19.5 17H4.5C4.22386 17 4 16.7762 4 16.5V8.50001C4 8.29778 4.12182 8.11547 4.30866 8.03807Z"
+        fill={iconColor}
+      />
     </svg>
   );
 }
