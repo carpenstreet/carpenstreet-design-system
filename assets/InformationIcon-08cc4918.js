734e619bb9fafc3501045a884e43cde78172866d
flynn0909 <flynn@acon3d.com>
2024-03-29 11:27:45 +0900

    Updates

--- assets/InformationIcon-08cc4918.js
+++
@@ -1,8 +0,0 @@
-import{j as o,a as i,F as e}from"./jsx-runtime-29545a09.js";import{M as a,C as s,b as c}from"./index-2dacff36.js";import{I as p,D as l}from"./InformationIcon.stories-15276d89.js";import{u as m}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-d4f2c5d8.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-ec879a92.js";import"./color-d4584655.js";import"./InformationIcon-9985c713.js";import"./useTheme-f5d04936.js";function r(n){const t=Object.assign({h1:"h1",ul:"ul",li:"li"},m(),n.components);return i(e,{children:[o(a,{of:p}),`
-`,o(t.h1,{id:"information-icon",children:"Information Icon"}),`
-`,i(t.ul,{children:[`
-`,o(t.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
-`,o(t.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
-`]}),`
-`,o(s,{of:l}),`
-`,o(c,{})]})}function L(n={}){const{wrapper:t}=Object.assign({},m(),n.components);return t?o(t,Object.assign({},n,{children:o(r,n)})):r(n)}export{L as default};
---
+++ assets/Tooltip-511860ce.js
@@ -0,0 +1,42 @@
+import{j as o,a as e,F as p}from"./jsx-runtime-29545a09.js";import{M as h,C as r,b as f,f as t}from"./index-012031e3.js";import{T as s,D as d,a as m,b as c,c as T,d as y,e as u,f as g,g as B,h as L,i as D,j as R,k as P,l as G,G as V,m as b,n as j,o as C,p as M,q as x,r as v,s as w,t as F,u as O,v as X,w as I,P as S,x as _,y as k,z as q,A as z,B as A,C as E,E as H,F as J,H as U,I as K,J as N}from"./Tooltip.stories-eae7e987.js";import{u as a}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-637f76d6.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./Typography-df3355a1.js";import"./ThemeMui-ec879a92.js";import"./color-d4584655.js";import"./generateUtilityClasses-49df5a04.js";import"./styled-823021aa.js";import"./useTheme-f5d04936.js";import"./useId-45bedd50.js";import"./useForkRef-2c2f4318.js";import"./useEnhancedEffect-92a32ebf.js";import"./mergeSlotProps-e22fd0e7.js";import"./isHostComponent-73d6e646.js";import"./TransitionGroupContext-319db280.js";import"./useControlled-3e5b2082.js";import"./Box-9631347a.js";function n(l){const i=Object.assign({h1:"h1",p:"p",h3:"h3",ul:"ul",li:"li",hr:"hr"},a(),l.components);return e(p,{children:[o(h,{of:s}),`
+`,o(i.h1,{id:"tooltip",children:"Tooltip"}),`
+`,o(i.p,{children:`사용자가 특정 요소 위로 마우스를 호버하면 Typography를 감싼 박스를 노출하는 컴포넌트입니다.
+MUI의 툴팁을 토대로 스타일을 override하여 제작되었습니다.`}),`
+`,o(i.h3,{id:"tooltip의-주요-prop",children:"Tooltip의 주요 Prop"}),`
+`,e(i.ul,{children:[`
+`,o(i.li,{children:"title: 툴팁 내에 들어갈 텍스트를 지정하고 Typography 컴포넌트로 감싸져 있습니다."}),`
+`,o(i.li,{children:'direction: mui의 placement와 동일하며, 디자인 시스템 피그마에 있는 툴팁의 Prop과 값이 일치합니다. direction이 "none"일 경우 arrow 아이콘이 사라집니다.'}),`
+`,o(i.li,{children:"color: 툴팁의 배경색을 지정합니다. 기본적으로 default 값이 적용됩니다."}),`
+`]}),`
+`,o(r,{of:d,layout:"centered"}),`
+`,o(f,{include:["title","direction","color"]}),`
+`,o(i.hr,{}),`
+`,o(i.h1,{id:"default",children:"Default"}),`
+`,o(i.h3,{id:"top",children:"Top"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:m}),o(t,{of:c}),o(t,{of:T})]}),`
+`,o(i.h3,{id:"left",children:"Left"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:y}),o(t,{of:u}),o(t,{of:g})]}),`
+`,o(i.h3,{id:"bottom",children:"Bottom"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:B}),o(t,{of:L}),o(t,{of:D})]}),`
+`,o(i.h3,{id:"right",children:"Right"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:R}),o(t,{of:P}),o(t,{of:G})]}),`
+`,o(i.hr,{}),`
+`,o(i.h1,{id:"grayishviolet",children:"GrayishViolet"}),`
+`,o(i.h3,{id:"top-1",children:"Top"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:V}),o(t,{of:b}),o(t,{of:j})]}),`
+`,o(i.h3,{id:"left-1",children:"Left"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:C}),o(t,{of:M}),o(t,{of:x})]}),`
+`,o(i.h3,{id:"bottom-1",children:"Bottom"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:v}),o(t,{of:w}),o(t,{of:F})]}),`
+`,o(i.h3,{id:"right-1",children:"Right"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:O}),o(t,{of:X}),o(t,{of:I})]}),`
+`,o(i.hr,{}),`
+`,o(i.h1,{id:"primary",children:"Primary"}),`
+`,o(i.h3,{id:"top-2",children:"Top"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:S}),o(t,{of:_}),o(t,{of:k})]}),`
+`,o(i.h3,{id:"left-2",children:"Left"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:q}),o(t,{of:z}),o(t,{of:A})]}),`
+`,o(i.h3,{id:"bottom-2",children:"Bottom"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:E}),o(t,{of:H}),o(t,{of:J})]}),`
+`,o(i.h3,{id:"right-2",children:"Right"}),`
+`,e(r,{layout:"centered",children:[o(t,{of:U}),o(t,{of:K}),o(t,{of:N})]})]})}function Vo(l={}){const{wrapper:i}=Object.assign({},a(),l.components);return i?o(i,Object.assign({},l,{children:o(n,l)})):n(l)}export{Vo as default};
