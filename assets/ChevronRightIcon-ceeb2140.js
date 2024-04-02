1647f360d784a5a454e569d7b2aad7e8a6ea6f53
flynn0909 <flynn@acon3d.com>
2024-04-02 14:14:53 +0900

    Updates

--- assets/ChevronRightIcon-ceeb2140.js
+++
@@ -1,8 +0,0 @@
-import{j as t,a as i,F as m}from"./jsx-runtime-29545a09.js";import{M as s,C as a,b as c}from"./index-04096b7c.js";import{C as p,D as h}from"./ChevronRightIcon.stories-c557505a.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-c61d1221.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-ec879a92.js";import"./color-d4584655.js";import"./ChevronRightIcon-9bfd5861.js";import"./useTheme-f5d04936.js";function r(n){const o=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),n.components);return i(m,{children:[t(s,{of:p}),`
-`,t(o.h1,{id:"chevronright-icon",children:"ChevronRight Icon"}),`
-`,i(o.ul,{children:[`
-`,t(o.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
-`,t(o.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
-`]}),`
-`,t(a,{of:h}),`
-`,t(c,{})]})}function y(n={}){const{wrapper:o}=Object.assign({},e(),n.components);return o?t(o,Object.assign({},n,{children:t(r,n)})):r(n)}export{y as default};
