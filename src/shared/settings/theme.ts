6a095d780dde1e56247e99ba4d627fb7b4313db5
flynn0909 <flynn@acon3d.com>
2024-04-02 17:13:35 +0900

    기본 breakpoints 복구

--- src/shared/settings/theme.ts
+++ src/shared/settings/theme.ts
@@ -71,11 +71,6 @@ declare module '@mui/material/styles' {
   interface TypographyVariantsOptions extends Partial<CustomTypographyVariants> {}
 
   interface BreakpointOverrides {
-    xs: false;
-    sm: false;
-    md: false;
-    lg: false;
-    xl: false;
     mobile: true;
     desktop: true;
   }
