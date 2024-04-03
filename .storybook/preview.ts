938934378ba8fc0fb03f99e31901b707d7e8c8a4
add0215 <mingeon@acon3d.com>
2024-04-03 15:25:48 +0900

    컴포넌트 반응형 처리에 관한 가이드문서 생성

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -11,7 +11,7 @@ const preview: Preview = {
     },
     options: {
       storySort: {
-        order: ['Intro', 'Foundation', 'Component'],
+        order: ['Intro', 'Developer', 'Foundation', 'Component'],
       },
     },
   },
