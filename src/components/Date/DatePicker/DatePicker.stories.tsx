2f67f434b4c9089feb397f3754bfed58438bbf87
flynn0909 <flynn@acon3d.com>
2024-04-11 14:40:08 +0900

    mdx 및 stories 수정

--- src/components/Date/DatePicker/DatePicker.stories.tsx
+++ src/components/Date/DatePicker/DatePicker.stories.tsx
@@ -20,5 +20,70 @@ type Story = StoryObj<typeof ExampleDatePicker>;
 export const Default: Story = {
   args: {
     locale: 'ko',
+    sx: {},
+    toolbarSx: {},
+    dayLabelSx: {},
+    monthPickerSx: {},
+    yearPickerSx: {},
+    contentSx: {},
+  },
+};
+
+export const Container: Story = {
+  args: {
+    locale: 'ko',
+    sx: {
+      border: `2px solid #4e4eff`,
+    },
+  },
+};
+
+export const Toolbar: Story = {
+  args: {
+    locale: 'ko',
+    toolbarSx: {
+      border: `2px solid #4e4eff`,
+      borderRadius: '8px',
+    },
+  },
+};
+
+export const DayLabel: Story = {
+  args: {
+    locale: 'ko',
+    dayLabelSx: {
+      border: `2px solid #4e4eff`,
+      borderRadius: '8px',
+    },
+  },
+};
+
+export const MonthPicker: Story = {
+  args: {
+    locale: 'ko',
+    monthPickerSx: {
+      border: `2px solid #4e4eff`,
+      borderRadius: '8px',
+    },
+  },
+};
+
+export const YearPicker: Story = {
+  args: {
+    locale: 'ko',
+    yearPickerSx: {
+      border: `2px solid #4e4eff`,
+      borderRadius: '8px',
+    },
+  },
+};
+
+export const Content: Story = {
+  args: {
+    locale: 'ko',
+    contentSx: {
+      border: `2px solid #4e4eff`,
+      borderRadius: '8px',
+    },
   },
 };
