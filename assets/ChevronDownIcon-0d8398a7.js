9e072fa3602fd735cfff54f98e9d801d573d36d3
flynn0909 <flynn@acon3d.com>
2024-04-12 16:58:20 +0900

    Updates

---
+++ assets/ChevronDownIcon-0d8398a7.js
@@ -0,0 +1,8 @@
+import{j as o,a as r,F as m}from"./jsx-runtime-29545a09.js";import{M as s,C as a,b as c}from"./index-16a61921.js";import{C as p,D as l}from"./ChevronDownIcon.stories-a2456e99.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-65fb4f92.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-ea0543e5.js";import"./color-d4584655.js";import"./ChevronDownIcon-46bcc75c.js";import"./useTheme-9256e830.js";function i(t){const n=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),t.components);return r(m,{children:[o(s,{of:p}),`
+`,o(n.h1,{id:"chevrondown-icon",children:"ChevronDown Icon"}),`
+`,r(n.ul,{children:[`
+`,o(n.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
+`,o(n.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
+`]}),`
+`,o(a,{of:l}),`
+`,o(c,{})]})}function L(t={}){const{wrapper:n}=Object.assign({},e(),t.components);return n?o(n,Object.assign({},t,{children:o(i,t)})):i(t)}export{L as default};
