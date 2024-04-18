13eacc60d7b4cc56a646f51ace7cd63be5388923
flynn0909 <flynn@acon3d.com>
2024-04-18 14:58:53 +0900

    Updates

---
+++ assets/DateRangePicker-e0ce4cee.js
@@ -0,0 +1,27 @@
+import{j as r,a as e,F as a}from"./jsx-runtime-29545a09.js";import{M as p,C as o,b as d}from"./index-f1f98b44.js";import{D as h,a as l,C as m,T as s,b,M as f,Y as x,c as k}from"./DateRangePicker.stories-414af67d.js";import{u as c}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-d0fa5d2f.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-c8622a6c.js";import"./color-d4584655.js";import"./DateRangePicker-d8a9baf1.js";import"./YearPicker-42e84fc9.js";import"./IconButton-624baebc.js";import"./generateUtilityClasses-2f942639.js";import"./styled-edafd4c4.js";import"./ButtonBase-f2fe5abb.js";import"./TransitionGroupContext-0066119d.js";import"./useForkRef-2c2f4318.js";import"./useIsFocusVisible-b0073b2a.js";import"./ChevronLeftIcon-d8d97fc7.js";import"./useTheme-f807ec51.js";import"./Button-a7abd3b1.js";import"./Typography-4f484d60.js";import"./CaretUpIcon-58896723.js";import"./CaretDownIcon-04db28ef.js";import"./ChevronRightIcon-e40912f9.js";import"./Box-2c208943.js";function t(i){const n=Object.assign({h1:"h1",p:"p",h2:"h2",code:"code",h3:"h3"},c(),i.components);return e(a,{children:[r(p,{of:h}),`
+`,r(n.h1,{id:"daterangepicker",children:"DateRangePicker"}),`
+`,r(n.p,{children:"기간을 선택할 수 있는 달력 컴포넌트입니다."}),`
+`,r(o,{of:l}),`
+`,r(d,{include:["locale","sx","toolbarSx","dayLabelSx","monthPickerSx","yearPickerSx","contentSx"]}),`
+`,r(n.h2,{id:"구성",children:"구성"}),`
+`,e(n.p,{children:["크게 ",r(n.code,{children:"Container"}),", ",r(n.code,{children:"Toolbar"}),", ",r(n.code,{children:"DayLabel"}),", ",r(n.code,{children:"MonthPicker"}),", ",r(n.code,{children:"YearPicker"}),", ",r(n.code,{children:"Content"}),"로 구성되어 있으며 각각의 영역에 커스텀 스타일을 지정할 수 있습니다."]}),`
+`,r(n.h3,{id:"container",children:"Container"}),`
+`,e(n.p,{children:["전체 요소를 감싸는 영역입니다. ",r(n.code,{children:"sx"})," prop을 통해 스타일을 지정할 수 있습니다."]}),`
+`,r(o,{of:m}),`
+`,r(n.h3,{id:"toolbar",children:"Toolbar"}),`
+`,e(n.p,{children:["상단 툴바입니다. ",r(n.code,{children:"toolbarSx"})," prop을 통해 스타일을 지정할 수 있습니다."]}),`
+`,r(o,{of:s}),`
+`,r(n.h3,{id:"daylabel",children:"DayLabel"}),`
+`,e(n.p,{children:["요일을 표시하는 영역입니다. ",r(n.code,{children:"dayLabelSx"})," prop을 통해 스타일을 지정할 수 있습니다."]}),`
+`,r(o,{of:b}),`
+`,r(n.h3,{id:"monthpicker",children:"MonthPicker"}),`
+`,r(n.p,{children:"월을 선택할 수 있는 영역입니다. 툴바에서 월을 클릭하면 표시됩니다. 다시 클릭하면 해당 영역이 사라집니다."}),`
+`,e(n.p,{children:[r(n.code,{children:"monthPickerSx"})," prop을 통해 스타일을 지정할 수 있습니다."]}),`
+`,r(o,{of:f}),`
+`,r(n.h3,{id:"yearpicker",children:"YearPicker"}),`
+`,r(n.p,{children:"년도를 선택할 수 있는 영역입니다. 툴바에서 년도를 클릭하면 표시됩니다. 다시 클릭하면 해당 영역이 사라집니다."}),`
+`,e(n.p,{children:[r(n.code,{children:"yearPickerSx"})," prop을 통해 스타일을 지정할 수 있습니다."]}),`
+`,r(o,{of:x}),`
+`,r(n.h3,{id:"content",children:"Content"}),`
+`,e(n.p,{children:["날짜를 선택할 수 있는 영역입니다. ",r(n.code,{children:"contentSx"})," prop을 통해 스타일을 지정할 수 있습니다."]}),`
+`,r(o,{of:k})]})}function Z(i={}){const{wrapper:n}=Object.assign({},c(),i.components);return n?r(n,Object.assign({},i,{children:r(t,i)})):t(i)}export{Z as default};
