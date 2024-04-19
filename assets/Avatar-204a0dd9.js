ac4556c3c2f3015b389845c74206edd4857956f9
flynn0909 <flynn@acon3d.com>
2024-04-19 15:40:16 +0900

    Updates

---
+++ assets/Avatar-204a0dd9.js
@@ -0,0 +1,8 @@
+import{j as o,a as i,F as c}from"./jsx-runtime-29545a09.js";import{M as a,C as m,b as p}from"./index-91b84c46.js";import{A as s,D as d}from"./Avatar.stories-2a740bf6.js";import{u as n}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-2e17c6c5.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ProfileFilledIcon-82c21b3f.js";import"./useTheme-efc0693e.js";import"./ThemeMui-aad13025.js";import"./color-d4584655.js";import"./generateUtilityClasses-5c0f82e6.js";import"./createSvgIcon-152e12bb.js";import"./styled-a23d82f7.js";import"./mergeSlotProps-9651d6f8.js";import"./useForkRef-2c2f4318.js";import"./Box-dcab4ca5.js";function e(t){const r=Object.assign({h1:"h1",p:"p",code:"code"},n(),t.components);return i(c,{children:[o(a,{of:s}),`
+`,o(r.h1,{id:"avatar",children:"Avatar"}),`
+`,o(r.p,{children:"유저의 프로필을 표시하는 UI입니다."}),`
+`,i(r.p,{children:[o(r.code,{children:"src"})," prop에 image url을 입력하여 프로필 이미지를 표시할 수 있으며, ",o(r.code,{children:"src"}),"를 입력하지 않으면 ",o(r.code,{children:"ProfileFilled"})," 아이콘이 표시됩니다."]}),`
+`,i(r.p,{children:[o(r.code,{children:"src"}),"에 입력된 image url을 통해 이미지를 받아오는데 실패하면 ",o(r.code,{children:"alt"}),"에 입력한 텍스트의 첫 글자가 안내됩니다."]}),`
+`,i(r.p,{children:["MUI ",o(r.code,{children:"<Avatar>"})," 컴포넌트의 다른 prop 또한 사용할 수 있습니다."]}),`
+`,o(m,{of:d}),`
+`,o(p,{include:["src","alt","sx"]})]})}function q(t={}){const{wrapper:r}=Object.assign({},n(),t.components);return r?o(r,Object.assign({},t,{children:o(e,t)})):e(t)}export{q as default};
