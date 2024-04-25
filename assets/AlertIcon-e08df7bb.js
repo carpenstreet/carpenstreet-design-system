47b6c68f56f7ad023bd851ac99a85637ba509ba5
flynn0909 <flynn@acon3d.com>
2024-04-25 14:39:06 +0900

    Updates

--- assets/AlertIcon-e08df7bb.js
+++
@@ -1,8 +0,0 @@
-import{j as t,a as r,F as m}from"./jsx-runtime-29545a09.js";import{M as s,C as a,b as c}from"./index-27a78c38.js";import{A as l,D as p}from"./AlertIcon.stories-ce2cc271.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-47cd2ebe.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-aad13025.js";import"./color-d4584655.js";import"./AlertIcon-6a978236.js";import"./useTheme-efc0693e.js";function i(n){const o=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),n.components);return r(m,{children:[t(s,{of:l}),`
-`,t(o.h1,{id:"alert-icon",children:"Alert Icon"}),`
-`,r(o.ul,{children:[`
-`,t(o.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
-`,t(o.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
-`]}),`
-`,t(a,{of:p}),`
-`,t(c,{})]})}function y(n={}){const{wrapper:o}=Object.assign({},e(),n.components);return o?t(o,Object.assign({},n,{children:t(i,n)})):i(n)}export{y as default};
