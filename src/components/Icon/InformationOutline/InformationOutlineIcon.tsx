6bd92ff51cba08f51b2228bb11f85ab7bd6ae30a
flynn0909 <flynn@acon3d.com>
2024-04-29 10:26:24 +0900

    fix InformationOutlineIcon

--- src/components/Icon/InformationOutline/InformationOutlineIcon.tsx
+++ src/components/Icon/InformationOutline/InformationOutlineIcon.tsx
@@ -7,7 +7,7 @@ export default function InformationOutlineIcon({ width = 24, height = 24, color 
   const iconColor = theme.palette[color];
 
   return (
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width={width} height={height} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         fillRule="evenodd"
         clipRule="evenodd"
