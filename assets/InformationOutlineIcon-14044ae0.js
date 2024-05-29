499c92605b9186357ce132a40d2ccce616cfa6ec
junoryu <juno@acon3d.com>
2024-05-29 11:26:57 +0900

    Updates

--- assets/InformationOutlineIcon-14044ae0.js
+++
@@ -1,8 +0,0 @@
-import{j as o,a as i,F as m}from"./jsx-runtime-29545a09.js";import{M as a,C as s,b as c}from"./index-30476f73.js";import{I as l,D as p}from"./InformationOutlineIcon.stories-ad1cc061.js";import{u as e}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-ee92e69d.js";import"../sb-preview/runtime.js";import"./extends-623938b0.js";import"./index-d3ea75b5.js";import"./index-11d98b33.js";import"./assertThisInitialized-081f9914.js";import"./inheritsLoose-c82a83d4.js";import"./index-8fd8397b.js";import"./index-356e4a49.js";import"./ThemeMui-2ef3ce30.js";import"./color-d4584655.js";import"./InformationOutlineIcon-defdd612.js";import"./useTheme-15712d8b.js";function r(n){const t=Object.assign({h1:"h1",ul:"ul",li:"li"},e(),n.components);return i(m,{children:[o(a,{of:l}),`
-`,o(t.h1,{id:"informationoutline-icon",children:"InformationOutline Icon"}),`
-`,i(t.ul,{children:[`
-`,o(t.li,{children:"width와 height을 이용해 아이콘의 크기를 결정합니다."}),`
-`,o(t.li,{children:"color는 디자인 시스템에 정의된 색상만 사용됩니다."}),`
-`]}),`
-`,o(s,{of:p}),`
-`,o(c,{})]})}function L(n={}){const{wrapper:t}=Object.assign({},e(),n.components);return t?o(t,Object.assign({},n,{children:o(r,n)})):r(n)}export{L as default};
