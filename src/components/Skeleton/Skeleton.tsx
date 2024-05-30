ebda0efc408464743643a4f9175cba92424b34cb
add0215 <88374916+add0215@users.noreply.github.com>
2024-05-30 15:45:36 +0900

    Skeleton QA 수정 (#74)
    * v1.19.0
    
    * circular 유형의 skeleton은 width 설정 시 height도 동일하게 적용되도록 수정

--- src/components/Skeleton/Skeleton.tsx
+++ src/components/Skeleton/Skeleton.tsx
@@ -2,5 +2,7 @@ import { Skeleton as MUISkeleton } from '@mui/material';
 import { SkeletonPropsType } from './Skeleton.types.ts';
 
 export default function Skeleton(props: SkeletonPropsType) {
-  return <MUISkeleton {...props} />;
+  const { width, height, variant = 'text', ...otherProps } = props;
+  const computedHeight = variant === 'circular' ? width : height;
+  return <MUISkeleton width={width} height={computedHeight} variant={variant} {...otherProps} />;
 }
